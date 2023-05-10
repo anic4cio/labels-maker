a496ab23aef4e0bd406060c0edd01235fa4fa890
anic4cio <anicaciovinicius36@gmail.com>
2023-05-10 18:08:09 -0300

    chore: script to test annotation labels

---
+++ src/test.js
@@ -0,0 +1,29 @@
+const pdfBuilder = require('./PDFBuilderDani')
+const fs = require('fs');
+
+
+
+
+
+(async () => {
+
+  const text = fs.readFileSync('./averba.txt', 'utf8').trim()
+  const regex = /\r\n|\n/gi
+  const arrayText = text.split(regex)
+
+  const dismissBlankLines = (textLine) => textLine.trim() !== ''
+  const fixedText = arrayText.filter(dismissBlankLines)
+
+  const formattedLabels = fixedText.map(text => {
+    return `${text}
+03 de maio de 2023
+
+Daniela dos Santos
+Oficial Auxiliar`
+  })
+  console.log(formattedLabels.length)
+  
+  const pdfString = await pdfBuilder(formattedLabels)
+  fs.writeFile('averba.pdf', pdfString, () => {})
+  
+})()
